0a75be6fbea3e0d973ae8799a23cd827b9a10d26
Julie Cassandra Gerner Bissø <jbissoe@gmail.com>
2026-03-09 13:37:29 +0100

    made a animation for the button

--- my-app/src/app/Home/page.jsx
+++ my-app/src/app/Home/page.jsx
@@ -1,10 +1,10 @@
 
 
-
 export default function home(){
 
     return(
         <>
+
         <h1>suck it loser</h1>
         </>
     )
--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,10 +1,11 @@
 "use client";
 
 /* imports */
+
 import "./loader.scss"
+import "./btnAnimation.scss"
 import Link from "next/link";
 import { useEffect, useState } from "react";
-
 
 /* images are made in scss file and put into an array */
 const backgrounds = ["bg1", "bg2"];
@@ -25,10 +26,10 @@ export default function Loader() {
     depending on what number is from that(so if closet to 0/1 then it's bg1) */
   }, []);
 
+
   return (
     <>
       <main>
-
 
         <section className={`background-image-container ${randomImage}`}>
           <div className="loader-content-container">
@@ -38,13 +39,16 @@ export default function Loader() {
               <h2>Train like a pro</h2>
             </div>
 
-
           </div>
 
-            <Link href="/Home" className="BtnToHomeFromLoader">Start training</Link>
-       
- 
-
+          <button className="BtnToHomeFromLoader">
+            <Link
+              href="/Home"
+              className="btn-flip BtnToHomeFromLoader"
+              data-front="Start training"
+              data-back="Welcome">
+            </Link>
+          </button>
         </section>
       </main>
     </>
